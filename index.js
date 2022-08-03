f8b10bcbc1563abd47776cf6abc12db5b1e9aa60
rikiarizona <rikiarizona@gmail.com>
2022-08-03 10:33:54 +0700

    TUGAS 30 JAVASCRIPT
    saya kirimkan tugas niomic

---
+++ index.js
@@ -0,0 +1,5 @@
+function regex(value) {
+    var ambilData = value.match(/[es]/g);
+    console.log(ambilData);
+}
+regex("Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
